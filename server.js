cf9d78334930e3068790ee68d8b1d768be2e0e8c
nguyennhutdang0711 <dangck24v7k537@vlvh.ctu.edu.vn>
2026-08-16 13:27:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server đang chạy trên cổng ${PORT}.`);
-});
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Kết nối thành công đến MongoDB.");
+
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server đang chạy trên cổng ${PORT}.`);
+        });
+    } catch (error) {
+        console.error("Lỗi khi khởi động server:", error);
+        process.exit();
+    }
+}
+
+startServer();
