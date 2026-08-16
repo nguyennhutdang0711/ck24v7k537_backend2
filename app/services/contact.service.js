ec35061daafedee1d8a96b01cc5c5ab49ed71a30
nguyennhutdang0711 <dangck24v7k537@vlvh.ctu.edu.vn>
2026-08-16 14:13:16 +0700

    Update contact.service.js

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -4,6 +4,27 @@ class ContactService {
     constructor(client) {
         this.Contact = client.db().collection("contacts");
     }
+
+    extractContactData(payload) {
+        const contact = {
+            name: payload.name,
+            email: payload.email,
+            address: payload.address,
+            phone: payload.phone,
+            favorite: payload.favorite,
+        };
+        return contact;
+    }
+
+    async create(payload) {
+        const contact = this.extractContactData(payload);
+        const result = await this.Contact.findOneAndUpdate(
+            contact,
+            { $set: { favorite: contact.favorite === true } },
+            { returnDocument: "after", upsert: true }
+        );
+        return result.value;
+    }
 }
 
 module.exports = ContactService;
